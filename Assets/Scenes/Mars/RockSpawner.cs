43c2f4b83ac7693bdd346dfa6640a9d37196ed66
anka6130 <59932615+anka6130@users.noreply.github.com>
2022-10-07 17:06:17 -0600

    Mars, Jupiter, Neptune
    Added scenes, assets, sound, camera animation

---
+++ Assets/Scenes/Mars/RockSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSpawner : MonoBehaviour
+{
+    public Rigidbody[] rocks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      for (int i = 0; i < 100; i++)
+      {
+        for (int j = 0; j < rocks.Length; j++)
+        {
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(0, 1000), 75, Random.Range(-1000, 0));
+            Rigidbody newRock = rocks[j];
+            newRock.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+            Instantiate(newRock, randomSpawnPosition, Quaternion.identity);
+        }
+      }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
